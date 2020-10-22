323cf510e1d6962073b9d763055ea47f786f9697
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2020-10-23 05:20:41 +0900

    refactor

--- sudoku.go
+++ sudoku.go
@@ -10,46 +10,7 @@ import (
 
 // Table is sudoku's table
 type Table struct {
-	_panels [9][9][]int
-}
-
-var lrtable = map[int]map[rune][]int{
-	0: {
-		'r': []int{1, 2},
-		'l': []int{3, 6},
-	},
-	1: {
-		'r': []int{0, 2},
-		'l': []int{4, 7},
-	},
-	2: {
-		'r': []int{0, 1},
-		'l': []int{5, 8},
-	},
-	3: {
-		'r': []int{4, 5},
-		'l': []int{0, 6},
-	},
-	4: {
-		'r': []int{3, 5},
-		'l': []int{1, 7},
-	},
-	5: {
-		'r': []int{3, 4},
-		'l': []int{2, 8},
-	},
-	6: {
-		'r': []int{7, 8},
-		'l': []int{0, 3},
-	},
-	7: {
-		'r': []int{6, 8},
-		'l': []int{1, 4},
-	},
-	8: {
-		'r': []int{6, 7},
-		'l': []int{2, 5},
-	},
+	panels [9][9]map[int]struct{}
 }
 
 func Parse(s string) (*Table, error) {
@@ -58,23 +19,7 @@ func Parse(s string) (*Table, error) {
 		return nil, fmt.Errorf("invalid rows count: want 9, but %d", len(rows))
 	}
 
-	panels := [][]map[int]int{
-		{
-			{},
-			{},
-			{},
-		},
-		{
-			{},
-			{},
-			{},
-		},
-		{
-			{},
-			{},
-			{},
-		},
-	}
+	panels := [9][9]map[int]struct{}{}
 
 	for i, v := range rows {
 		if len(v) != 9 {
@@ -86,232 +31,142 @@ func Parse(s string) (*Table, error) {
 				return nil, fmt.Errorf("invalid char: want ([1-9]|_), but %s", string(p))
 			}
 
-			if p == 95 {
-				panels[i/3][j/3][i%3*3+j%3] = 0
-			} else {
-				panels[i/3][j/3][i%3*3+j%3] = int(p) - 48
+			if panels[i][j] == nil {
+				panels[i][j] = map[int]struct{}{}
+			}
+
+			if p != 95 {
+				panels[i][j][int(p)-48] = struct{}{}
 			}
 		}
 	}
 
-	var _panels [9][9][]int
 	for i := range panels {
 		for j := range panels[i] {
+			if len(panels[i][j]) == 1 {
+				continue
+			}
+
 			p := map[int]struct{}{}
-			for k, v := range panels[i][j] {
-				if v != 0 {
-					_panels[i*3+j][k] = append(_panels[i*3+j][k], v)
-					p[v] = struct{}{}
+			for k := 0; k < 9; k++ {
+				if len(panels[i][k]) != 1 {
+					continue
+				}
+
+				for key := range panels[i][k] {
+					p[key] = struct{}{}
 				}
 			}
 
-			ac := []int{}
-			for c := 1; c <= 9; c++ {
-				if _, ok := p[c]; !ok {
-					ac = append(ac, c)
+			for k := 0; k < 9; k++ {
+				if len(panels[k][j]) != 1 {
+					continue
+				}
+
+				for key := range panels[k][j] {
+					p[key] = struct{}{}
 				}
 			}
 
-			for k, v := range panels[i][j] {
-				if v == 0 {
-					_panels[i*3+j][k] = make([]int, len(ac))
-					copy(_panels[i*3+j][k], ac)
+			_i := i / 3 * 3
+			_j := j / 3 * 3
+			for x := 0; x < 3; x++ {
+				for y := 0; y < 3; y++ {
+					if len(panels[_i+x][_j+y]) != 1 {
+						continue
+					}
+
+					for key := range panels[_i+x][_j+y] {
+						p[key] = struct{}{}
+					}
+				}
+			}
+
+			for k := 1; k <= 9; k++ {
+				if _, ok := p[k]; !ok {
+					panels[i][j][k] = struct{}{}
 				}
 			}
 		}
 	}
 
 	return &Table{
-		_panels: _panels,
+		panels: panels,
 	}, nil
 }
 
-func (t *Table) RemovalInPanel(i int) {
-	p := map[int]struct{}{}
-	for j := range t._panels[i] {
-		if len(t._panels[i][j]) == 1 {
-			p[t._panels[i][j][0]] = struct{}{}
-		}
-	}
-
-	ac := []int{}
-	for c := 1; c <= 9; c++ {
-		if _, ok := p[c]; !ok {
-			ac = append(ac, c)
-		}
-	}
-
-	for j := range t._panels[i] {
-		if len(t._panels[i][j]) != 1 {
-			t._panels[i][j] = make([]int, len(ac))
-			copy(t._panels[i][j], ac)
-		}
-	}
-}
-
-func (t *Table) Removal() {
-	for i := range t._panels {
-		for j := range t._panels[i] {
-			if len(t._panels[i][j]) == 1 {
-				continue
-			}
-
-			for _, p := range lrtable[i]['r'] {
-				for k := 0; k < 3; k++ {
-					t2 := t._panels[p][j/3*3+k]
-					if len(t2) == 1 {
-						p := []int{}
-						for _, c := range t._panels[i][j] {
-							if t2[0] != c {
-								p = append(p, c)
-							}
-						}
-						t._panels[i][j] = p
-					}
-				}
-			}
-
-			for _, p := range lrtable[i]['l'] {
-				for k := 0; k < 3; k++ {
-					t2 := t._panels[p][j%3+3*k]
-					if len(t2) == 1 {
-						p := []int{}
-						for _, c := range t._panels[i][j] {
-							if t2[0] != c {
-								p = append(p, c)
-							}
-						}
-						t._panels[i][j] = p
-					}
-				}
-			}
-		}
-	}
-}
-
 func (t *Table) Solve() error {
-	t.Removal()
 	return t.dfs(0, 0)
 }
 
 // TODO: improvement
 func (t *Table) dfs(i, j int) error {
-	if i == 9 && j == 0 {
-		return t.Check()
-	}
+	// if i == 9 && j == 0 {
+	// 	return t.Check()
+	// }
 
-	t.RemovalInPanel(i)
-	t.Removal()
+	// raw := make([]int, len(t._panels[i][j]))
+	// copy(raw, t._panels[i][j])
 
-	raw := make([]int, len(t._panels[i][j]))
-	copy(raw, t._panels[i][j])
+	// for _, can := range raw {
+	// 	t._panels[i][j] = []int{can}
 
-	for _, can := range raw {
-		t._panels[i][j] = []int{can}
+	// 	nexti := i
+	// 	nextj := j
 
-		nexti := i
-		nextj := j
+	// 	if j == 8 {
+	// 		nexti++
+	// 		nextj = 0
+	// 	} else {
+	// 		nextj++
+	// 	}
 
-		if j == 8 {
-			nexti++
-			nextj = 0
-		} else {
-			nextj++
-		}
+	// 	if err := t.dfs(nexti, nextj); err == nil {
+	// 		return nil
+	// 	}
+	// }
 
-		if err := t.dfs(nexti, nextj); err == nil {
-			return nil
-		}
-	}
+	// t._panels[i][j] = raw
 
-	t._panels[i][j] = raw
-
-	return fmt.Errorf("no")
+	return nil
 }
 
 func (t *Table) Check() error {
-	for i := range t._panels {
-		al := map[int]struct{}{}
-		for j := 0; j < 9; j++ {
-			p := t._panels[i][j]
-			if len(p) != 1 {
-				return fmt.Errorf("invalid count")
-			}
-
-			point := p[0]
-			if _, ok := al[point]; !ok {
-				al[point] = struct{}{}
-			} else {
-				return fmt.Errorf("already in panel")
-			}
-		}
-
-		for j := range t._panels[i] {
-			for _, p := range lrtable[i]['r'] {
-				for k := 0; k < 3; k++ {
-					c := t._panels[p][j/3*3+k]
-					if t._panels[i][j][0] == c[0] {
-						return fmt.Errorf("already in row")
-					}
-				}
-			}
-		}
-
-		for j := range t._panels[i] {
-			for _, p := range lrtable[i]['l'] {
-				for k := 0; k < 3; k++ {
-					c := t._panels[p][j%3+3*k]
-					if t._panels[i][j][0] == c[0] {
-						return fmt.Errorf("already in line")
-					}
-				}
-			}
-		}
-	}
-
 	return nil
 }
 
 func (t *Table) Render() string {
 	writer := table.NewWriter()
 
-	r := table.Row{}
-	for _, v := range t._panels {
+	l := []string{}
+	for i := range t.panels {
 		s := ""
-		for _, p := range v {
-			if len(p) == 1 {
-				s += strconv.Itoa(p[0])
+		for j := range t.panels[i] {
+			if len(t.panels[i][j]) == 1 {
+				for key := range t.panels[i][j] {
+					s += strconv.Itoa(key)
+				}
 			} else {
 				s += "_"
 			}
-
-			if len(s) == 3 || len(s) == 7 {
-				s += "\n"
-			}
-
-			if len(s) == 11 {
-				r = append(r, s)
-			}
 		}
 
-		if len(r) == 3 {
+		l = append(l, s)
+		if len(l) == 3 {
+			r := table.Row{}
+			for k := 0; k < 9; k += 3 {
+				a := string(l[0][k]) + string(l[0][k+1]) + string(l[0][k+2])
+				b := string(l[1][k]) + string(l[1][k+1]) + string(l[1][k+2])
+				c := string(l[2][k]) + string(l[2][k+1]) + string(l[2][k+2])
+
+				r = append(r, a+"\n"+b+"\n"+c)
+			}
 			writer.AppendRow(r)
-			r = table.Row{}
+			l = []string{}
 		}
 	}
 
 	writer.Style().Options.SeparateRows = true
 
 	return writer.Render()
-}
-
-func (t *Table) Yoko() string {
-	s := ""
-	for i := range t._panels {
-		for j := range t._panels[i] {
-			s += strconv.Itoa(t._panels[i][j][0])
-		}
-	}
-
-	return s
 }
